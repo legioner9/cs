fd53ee7413d6248ba1615d0e901039cd047ac1ec
legioner9 <legioner9@inbox.ru>
2025-10-14 01:40:28 +0700

    <>

---
+++ .d/.tmp/wget5gitflic/wget5flic2fs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd "C:\YandexDisk\_repo\cs\.d\.tmp\wget5gitflic"
+
+# https://gitflic.ru/project/legioner9/st_rc_d/blob/raw?file=.d%2F.config%2Fenv.sh&inline=false&commit=76d19bd834e190d23c3911a4d0c2ac25fc93f908
+# https://gitflic.ru/project/legioner9/st_rc_d/blob/raw?file=.d%2F.config%2Fenv.sh
+# wget https://gitflic.ru/project/legioner9/st_rc_d/blob/raw?file=.d%2F.config%2Fenv.sh
+# WORK wget -O env.sh  https://gitflic.ru/project/legioner9/st_rc_d/blob/raw?file=.d%2F.config%2Fenv.sh
+
+ (curl --output cs.zip https://gitflic.ru/project/legioner9/cs/file/downloadAll?branch=master&format=zip)
