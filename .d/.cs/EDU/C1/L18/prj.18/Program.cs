d3b4752a0063f88fdd89f22b96497a0f4b374b0a
legioner9 <legioner9@inbox.ru>
2025-10-18 15:41:29 +0700

    <>

--- .d/.cs/EDU/C1/L18/prj.18/Program.cs
+++ .d/.cs/EDU/C1/L18/prj.18/Program.cs
@@ -2,34 +2,42 @@
 using System.Transactions;
 using prj;
 
+namespace prj
+{
+    class Program()
+    {
+        static void Main(string[] args)
+        {
+            Killer klr = new("13", 23, [33, 43, 53], 34);
 
-Killer klr = new("13", 23, [33, 43, 53], 34);
+            klr.Health = 100;
+            System.Console.WriteLine(klr.Health);
 
-klr.Health = 100;
-System.Console.WriteLine(klr.Health);
-
-List<Robot> rbts = [
-new("15", 25, [35, 45, 55]),
+            List<Robot> rbts = [
+            new("15", 25, [35, 45, 55]),
 new("16", 26, [36, 46, 56]),
 new("17", 27, [37, 47, 57])
-];
+            ];
 
-// foreach (Robot el in rbts)
-// {
-//     el.prnPrm();
-// }
+            // foreach (Robot el in rbts)
+            // {
+            //     el.prnPrm();
+            // }
 
-// var listEnumerator = rbts.GetEnumerator();
+            // var listEnumerator = rbts.GetEnumerator();
 
-// for (var i = 0; listEnumerator.MoveNext(); i++)
-// {
-//     Robot currRbt = listEnumerator.Current;
-//     Console.WriteLine("index i = {0}", i);
-//     currRbt.prnPrm();
-// }
+            // for (var i = 0; listEnumerator.MoveNext(); i++)
+            // {
+            //     Robot currRbt = listEnumerator.Current;
+            //     Console.WriteLine("index i = {0}", i);
+            //     currRbt.prnPrm();
+            // }
 
-foreach (Robot el in rbts)
-{
-    Console.WriteLine("index i = {0}", rbts.IndexOf(el));
-    el.prnPrm();
+            foreach (Robot el in rbts)
+            {
+                Console.WriteLine("index i = {0}", rbts.IndexOf(el));
+                el.prnPrm();
+            }
+        }
+    }
 }
