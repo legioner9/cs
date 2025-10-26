a4e92964bfc6a167f5c0278e7aa99b02a727c3d8
legioner9 <legioner9@inbox.ru>
2025-10-26 11:57:25 +0700

    <>

--- .d/.sh/rpo2git_with_bcp.sh
+++ .d/.sh/rpo2git_with_bcp.sh
@@ -3,8 +3,9 @@
 DDir=$(pwd)
 cd "C:\YandexDisk\_repo\cs"
 
+echo -e "${ECHO_TRUE}Rewrite Snippets VSCode :: only 'y'${NRM}"
 unset yes
-read -rpe "${ECHO_TRUE}Rewrite Snippets VSCode :: only 'y'${NRM}" yes
+read -r yes
 if [ "y" == "$yes" ]; then
     rm -rf ".d\.bcp_dpl\vs_code\Snippets"
     mkdir ".d\.bcp_dpl\vs_code\Snippets"
