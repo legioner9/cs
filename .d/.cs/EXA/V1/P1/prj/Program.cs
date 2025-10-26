5b9a5f2b1c19662e4f1e3a462df43cdc1d6b1d4c
legioner9 <legioner9@inbox.ru>
2025-10-26 22:54:02 +0700

    <>

--- .d/.cs/EXA/V1/P1/prj/Program.cs
+++ .d/.cs/EXA/V1/P1/prj/Program.cs
@@ -2,62 +2,39 @@
 using System;
 namespace YourNamespace
 {
-    class YourClass
-    {
-        Yanshoff y = new Yanshoff();
-        y.MyValue = "this is my value!";
-
-    y.GetType().GetProperties().ToList().ForEach(prop=>
-    {
-            var val = prop.GetValue(y, null);
-
-            System.Console.WriteLine("{0} : {1}", prop.Name, val);
-        });
-
-    y.GetType().GetMethods().ToList().ForEach(meth=>
-    {
-            System.Console.WriteLine(meth.Name);
-        }
-    }
-
-    struct YourStruct
-    {
-    }
-
-    interface IYourInterface
-    {
-    }
-
-    delegate int YourDelegate();
-
-    enum YourEnum
-    {
-    }
-
-    namespace YourNestedNamespace
-    {
-        struct YourStruct
-        {
-        }
-    }
-
-    public class Yanshoff
-{
-    public string MyValue {get; set;}
-
-    public void MyMethod()
-    {
-         System.Console.WriteLine("I'm a Method!");
-    }
-
-
-}
-
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Yanshoff y = new Yanshoff();
+            y.MyValue = "this is my value!";
+
+            y.GetType().GetProperties().ToList().ForEach(prop =>
+            {
+                var val = prop.GetValue(y, null);
+
+                System.Console.WriteLine("{0} : {1}", prop.Name, val);
+            });
+
+            y.GetType().GetMethods().ToList().ForEach(meth =>
+            {
+                System.Console.WriteLine(meth.Name);
+            });
+        }
+
+
+
+        public class Yanshoff
+        {
+            public string MyValue { get; set; }
+
+            public void MyMethod()
+            {
+                System.Console.WriteLine("I'm a Method!");
+            }
         }
     }
 }
+
+
+
