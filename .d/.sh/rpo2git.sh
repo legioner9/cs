f0699ca66192d7b368ec81c08e50f54ccda6e34f
legioner9 <legioner9@inbox.ru>
2025-10-26 11:30:17 +0700

    <>

--- .d/.sh/rpo2git.sh
+++ .d/.sh/rpo2git.sh
@@ -6,9 +6,9 @@ cd "C:\YandexDisk\_repo\cs"
 unset yes
 read -rp "Revrite Snippets VSCode :: only 'y'" yes
 if [ "y" == "$yes" ]; then
-    rm -f ".d\.bcp_dpl\vs_code\Snippets"
+    rm -rf ".d\.bcp_dpl\vs_code\Snippets"
     mkdir ".d\.bcp_dpl\vs_code\Snippets"
-    cp "C:\Users\ProNout\AppData\Roaming\Code\User\snippets" ".d\.bcp_dpl\vs_code\Snippets"
+    cp -r "C:\Users\ProNout\AppData\Roaming\Code\User\snippets" ".d\.bcp_dpl\vs_code\Snippets"
 fi
 
 git add .
