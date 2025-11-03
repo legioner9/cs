840762042efc8847326e148e0297a64220e89a61
legioner9 <legioner9@inbox.ru>
2025-11-03 22:19:08 +0700

    <>

--- .d/.cs/.arb/.man.arb/pnt.man.arb/out.ram/.grot/exa.d/exa_1.d/exa.cs
+++ .d/.cs/.arb/.man.arb/pnt.man.arb/out.ram/.grot/exa.d/exa_1.d/exa.cs
@@ -1,1 +1,15 @@
+string GetNextNameByOut(out int id)
+{
+    id = 1;
+    string returnText = "Next-" + id.ToString();
+    return returnText;
+}
+void Main()
+{
+    int i = 0;
+    Console.WriteLine("Previous value of integer i:" + i.ToString());
+    string test = GetNextNameByOut(out i);
+    Console.WriteLine("Current value of integer i:" + i.ToString());
+}
 
+Main();
