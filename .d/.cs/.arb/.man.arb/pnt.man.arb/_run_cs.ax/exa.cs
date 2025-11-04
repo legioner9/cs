0a27d3ba91c1d3ca4d92c57982b7e7660866b805
legioner9 <legioner9@inbox.ru>
2025-11-04 13:55:52 +0700

    <>

--- .d/.cs/.arb/.man.arb/pnt.man.arb/_run_cs.ax/exa.cs
+++ .d/.cs/.arb/.man.arb/pnt.man.arb/_run_cs.ax/exa.cs
@@ -1,2 +1,2 @@
-﻿int? a = null;
-Console.Write(a);
+﻿var v = new { fld1 = 100, fld2 = "wfwq" };
+Console.Write($"{v.fld1} , {v.fld2}");
---
+++ .d/.cs/.arb/.man.arb/pnt.man.arb/anon_type.ram/.grot/exa.d/exa_1.d/exa.cs
@@ -0,0 +1,2 @@
+var v = new { fld1 = 100, fld2 = "wfwq" };
+Console.Write($"{v.fld1} , {v.fld2}");
