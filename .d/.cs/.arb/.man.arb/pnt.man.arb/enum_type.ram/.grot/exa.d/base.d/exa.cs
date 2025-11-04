1f2c54603e8444eb4378f6e2febf25d8c84709c8
legioner9 <legioner9@inbox.ru>
2025-11-04 23:11:27 +0700

    <>

--- .d/.cs/.arb/.man.arb/pnt.man.arb/enum_type.ram/.grot/exa.d/base.d/exa.cs
+++
@@ -1,7 +0,0 @@
-enum ErrorCode : sbyte
-{
-    None = 0,
-    Unknown = 1,
-    ConnectionLonst = 100,
-    OutlierReader = 300
-}
---
+++ .d/.cs/.arb/.man.arb/pnt.man.arb/enum_type.ram/.grot/exa.d/base_2.d/exa.cs
@@ -0,0 +1,17 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(ErrorCode.OutlierReader);
+        Console.WriteLine((int)ErrorCode.OutlierReader);
+
+    }
+}
+
+enum ErrorCode : sbyte
+{
+    None = 0,
+    Unknown = 1,
+    ConnectionLonst = 100,
+    OutlierReader = 127
+}
