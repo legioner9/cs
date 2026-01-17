55c52bb4452971a1496168381196c89218b8b4de
legioner9 <legioner9@inbox.ru>
2026-01-17 16:49:42 +0700

    <>

---
+++ .d/.cs/EDU_solid_api/C_002/add_in_v1/SOLIDWORKS SDK Add-In Template (VB)1/AddIn.vb
@@ -0,0 +1,21 @@
+﻿'Copyright 2023 BLUE BYTE SYSTEMS INC.
+'Permission Is hereby granted, free Of charge, to any person obtaining a copy of this software And associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, And/Or sell copies of the Software, And to permit persons to whom the Software Is furnished to do so, subject to the following conditions:
+'The above copyright notice And this permission notice shall be included In all copies Or substantial portions Of the Software.
+'THE SOFTWARE Is PROVIDED "AS IS", WITHOUT WARRANTY Of ANY KIND, EXPRESS Or IMPLIED, INCLUDING BUT Not LIMITED To THE WARRANTIES Of MERCHANTABILITY, FITNESS For A PARTICULAR PURPOSE And NONINFRINGEMENT. In NO Event SHALL THE AUTHORS Or COPYRIGHT HOLDERS BE LIABLE For ANY CLAIM, DAMAGES Or OTHER LIABILITY, WHETHER In AN ACTION Of CONTRACT, TORT Or OTHERWISE, ARISING FROM, OUT Of Or In CONNECTION With THE SOFTWARE Or THE USE Or OTHER DEALINGS In THE SOFTWARE.
+
+Imports BlueByte.SOLIDWORKS.SDK.Attributes
+Imports BlueByte.SOLIDWORKS.SDK.Core
+Imports System
+Imports System.Runtime.InteropServices
+
+Namespace SOLIDWORKS_SDK_Add_In_Template__VB_1
+    <ComVisible(True)>
+    <Guid("c4e375f4-c640-44e2-8940-287d0e3283f8")>
+    <Name("SOLIDWORKS SDK Add-In Template (VB)1")>
+    <Description("SOLIDWORKS SDK Add-In Template (VB)1")>
+    <StartUp(True)>
+    Public Class AddIn
+        Inherits AddInBase
+    End Class
+End Namespace
+
---
+++ .d/.cs/EDU_solid_api/C_002/add_in_v1/SOLIDWORKS SDK Add-In Template (VB)1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SOLIDWORKS_SDK_Add_In_Template__VB_1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SOLIDWORKS_SDK_Add_In_Template__VB_1")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5feb00b6-83e8-463e-8473-38c64993d2c2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
