f73747a8303870ea5897d23da5e70bd0003a0dca
legioner9 <legioner9@inbox.ru>
2026-01-18 15:45:32 +0700

    <>

---
+++ .d/.cs/EDU_solid_api/YT_e_007/C001/L001/prj/SWXSTANDALONE/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SWXSTANDALONE
+{
+    internal static class Helper
+    {
+        internal static string[] GetCADFlsFrmDr(string dr)
+        {
+            // check isDirectory
+            if (!Directory.Exists(dr))
+               return null;
+            // get sldprts from dr
+            string[] prts = Directory.GetFiles(dr,"sldpart");
+            // get sldasm from dr
+            string[] asms = Directory.GetFiles(dr, "sldasm");
+
+            List<string> cadFiles = new List<string>();
+
+            // add to list necessary fl_nm
+            if (prts!=null)
+             cadFiles.AddRange(prts);
+            if (asms != null)
+                cadFiles.AddRange(asms);
+
+            // return list as array
+            return cadFiles.ToArray();
+        }
+    }
+}
--- .d/.cs/EDU_solid_api/YT_e_007/C001/L001/prj/SWXSTANDALONE_v001/Form1.cs
+++ .d/.cs/EDU_solid_api/YT_e_007/C001/L001/prj/SWXSTANDALONE_v001/Form1.cs
@@ -22,6 +22,7 @@ namespace SWXSTANDALONE
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // sync meth
             swApp = SolidWorksSinglton.getApplication();
         }
     }
